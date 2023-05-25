b3b8a1afeef89a0013e6d628a149a820f81517c8
Andres Cayambe <andrescayambe99@hotmail.com>
2023-05-25 21:04:29 +0200

    [add]class

---
+++ Models/movies.php
@@ -0,0 +1,14 @@
+<?php
+
+class Movie{
+  public $title;
+  public $duration;
+  public $poster;
+
+  public function __construct($_title, $_duration, $_poster)
+  {
+    $this->title = $_title;
+    $this->duration = $_duration;
+    $this->poster = $_poster;
+  }
+}
